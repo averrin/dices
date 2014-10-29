488da849c1ad249200ddbd030f5c6b1042c706ef
a.nabrodov <a.nabrodov@roscryptpro.ru>
2014-10-29 13:10:43 +0300

    some more websockets

--- main.go
+++ main.go
@@ -17,10 +17,11 @@ import (
 	"encoding/json"
 )
 
+
 type RollRecord struct {
-	DiceDef string
-	Roll int
-	Timestamp time.Time
+	DiceDef 	string		`json:"dice_def"`
+	Roll 		int			`json:"roll"`
+	Timestamp 	time.Time 	`json:"timestamp"`
 }
 
 func CreateRollRecord(pool *dices.DicePool, c *mgo.Collection) *RollRecord {
@@ -35,10 +36,58 @@ func CreateRollRecord(pool *dices.DicePool, c *mgo.Collection) *RollRecord {
 	return record
 }
 
+func WSHandler(w http.ResponseWriter, r *http.Request, c *mgo.Collection) {
+	log.Println(ws_helpers.ActiveClients)
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		log.Println(err)
+		return
+	}
+	client := ws.RemoteAddr()
+	pattern := `\dd\d+([\+-]\d+)?`
+	sockCli := ws_helpers.ClientConn{ws, client}
+	ws_helpers.AddClient(sockCli)
+
+	for {
+		var ret []byte;
+		log.Println(len(ws_helpers.ActiveClients), ws_helpers.ActiveClients)
+		messageType, p, err := ws.ReadMessage()
+		def := string(p)
+		if err != nil {
+			ws_helpers.DeleteClient(sockCli)
+			log.Println("bye")
+			log.Println(err)
+			return
+		}
+		match, _ := regexp.MatchString(pattern, def)
+		if match {
+			p := dices.CreateDicePool(def)
+			p.Roll()
+			r := struct {
+					Type string
+					Message *RollRecord
+				}{"roll", CreateRollRecord(p, c)}
+			ret, err = json.Marshal(r)
+			ws_helpers.BroadcastMessage(messageType, ret)
+		} else {
+			r := struct {
+					Type string
+					Message string
+				}{"error", "Wrong format!"}
+			ret, err = json.Marshal(r)
+			if err := ws.WriteMessage(messageType, ret); err != nil {
+				return
+			}
+		}
+	}
+}
+
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	pattern := `\dd\d+([\+-]\d+)?`
 	fmt.Println("Started")
 	m := martini.Classic()
 	m.Use(render.Renderer(render.Options{
@@ -54,48 +103,12 @@ func main() {
 	session.SetMode(mgo.Monotonic, true)
 
 	c := session.DB("test").C("rolls")
+	m.Map(c)
 	m.Get("/rolls", func(r render.Render) {
 		var rolls []RollRecord
 		c.Find(bson.M{}).Iter().All(&rolls)
-		fmt.Println(rolls)
 		r.HTML(200, "rolls", rolls)
 	})
-	m.Get("/sock", func(w http.ResponseWriter, r *http.Request) {
-			log.Println(ws_helpers.ActiveClients)
-			ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
-			if _, ok := err.(websocket.HandshakeError); ok {
-				http.Error(w, "Not a websocket handshake", 400)
-				return
-			} else if err != nil {
-				log.Println(err)
-				return
-			}
-			client := ws.RemoteAddr()
-			sockCli := ws_helpers.ClientConn{ws, client}
-			ws_helpers.AddClient(sockCli)
-			var ret []byte;
-
-			for {
-				log.Println(len(ws_helpers.ActiveClients), ws_helpers.ActiveClients)
-				messageType, p, err := ws.ReadMessage()
-				def := string(p)
-				if err != nil {
-					ws_helpers.DeleteClient(sockCli)
-					log.Println("bye")
-					log.Println(err)
-					return
-				}
-				match, _ := regexp.MatchString(pattern, def)
-				if match {
-					p := dices.CreateDicePool(def)
-					p.Roll()
-					r := CreateRollRecord(p, c)
-					ret, err = json.Marshal(r)
-				} else {
-					ret = []byte("Wrong format!")
-				}
-				ws_helpers.BroadcastMessage(messageType, ret)
-			}
-		})
+	m.Get("/sock", WSHandler)
 	m.Run()
 }
